35d08e9a52df1a652e2e28a5696dd7f5079c5d41
iUltimateLP <iredcraft@gmail.com>
2015-12-06 09:35:34 +0100

    Mini fix #2

--- Launchpad-LED-Editor/DeviceManager.cs
+++ Launchpad-LED-Editor/DeviceManager.cs
@@ -36,14 +36,14 @@ namespace Launchpad_LED_Editor
         };
 
         public static Pitch[,] sessionNotesS = new Pitch[8, 8] {
-            { Pitch.E3, Pitch.F3, Pitch.FSharp3, Pitch.G3, Pitch.C6, Pitch.CSharp6, Pitch.D6, Pitch.DSharp6 },
-            { Pitch.C3, Pitch.CSharp3, Pitch.D3, Pitch.DSharp3, Pitch.GSharp5, Pitch.A5, Pitch.ASharp5, Pitch.B5 },
-            { Pitch.GSharp2, Pitch.A2, Pitch.ASharp2, Pitch.B2, Pitch.E5, Pitch.F5, Pitch.FSharp5, Pitch.G5 },
-            { Pitch.E2, Pitch.F2, Pitch.FSharp2, Pitch.G2, Pitch.C5, Pitch.CSharp5, Pitch.D5, Pitch.DSharp5 },
-            { Pitch.C2, Pitch.CSharp2, Pitch.D2, Pitch.DSharp2, Pitch.GSharp4, Pitch.A4, Pitch.ASharp4, Pitch.B4 },
-            { Pitch.GSharp1, Pitch.A1, Pitch.ASharp1, Pitch.B1, Pitch.E4, Pitch.F4, Pitch.FSharp4, Pitch.G4 },
-            { Pitch.E1, Pitch.F1, Pitch.FSharp1, Pitch.G1, Pitch.C4, Pitch.CSharp4, Pitch.D4, Pitch.DSharp4 },
-            { Pitch.C1, Pitch.CSharp1, Pitch.D1, Pitch.DSharp1, Pitch.GSharp3, Pitch.A3, Pitch.ASharp3, Pitch.B3 },
+            { Pitch.CNeg1, Pitch.CSharpNeg1, Pitch.DNeg1, Pitch.DSharpNeg1, Pitch.ENeg1, Pitch.FNeg1, Pitch.FSharpNeg1, Pitch.GNeg1},
+            { Pitch.E0, Pitch.F0, Pitch.FSharp0, Pitch.G0, Pitch.GSharp0, Pitch.A0, Pitch.ASharp0, Pitch.B0},
+            { Pitch.GSharp1, Pitch.A1, Pitch.ASharp1, Pitch.B1, Pitch.C2, Pitch.CSharp2, Pitch.D2, Pitch.DSharp2},
+            { Pitch.C3, Pitch.CSharp3, Pitch.D3, Pitch.DSharp3, Pitch.E3, Pitch.F3, Pitch.FSharp3, Pitch.G3},
+            { Pitch.E4, Pitch.F4, Pitch.FSharp4, Pitch.G4, Pitch.GSharp4, Pitch.A4, Pitch.ASharp4, Pitch.B4},
+            { Pitch.GSharp5, Pitch.A5, Pitch.ASharp5, Pitch.B5, Pitch.C6, Pitch.CSharp6, Pitch.D6, Pitch.DSharp6},
+            { Pitch.C7, Pitch.CSharp7, Pitch.D7, Pitch.DSharp7, Pitch.E7, Pitch.F7, Pitch.FSharp7, Pitch.G7},
+            { Pitch.E8, Pitch.F8, Pitch.FSharp8, Pitch.G8, Pitch.GSharp8, Pitch.A8, Pitch.ASharp8, Pitch.B8},
         };
 
         public static Pitch[] sideLEDnotes = new Pitch[]
